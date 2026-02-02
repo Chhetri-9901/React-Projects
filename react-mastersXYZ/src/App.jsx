09919d1cd7c28847bc9a523f26883220eb05d492
Chhetri-9901 <vivekchhetri9901@gmail.com>
2026-02-02 22:50:51 +0530

    Mirror text day 1

--- react-mastersXYZ/src/App.jsx
+++ react-mastersXYZ/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Toggle_Visibility from './components/Toggle_Visibility'
 import Counter from './components/Counter'
+import Mirror_text from './components/Mirror_text'
 
 const App = () => {
   return (
     <>
       <Toggle_Visibility />
       <Counter />
+      <Mirror_text />
     </>
   )
 }
---
+++ react-mastersXYZ/src/components/Mirror_text.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Mirror_text = () => {
+  return <div className="max-w-xl w-100% mt-4 border-white p-4 flex flex-col justify-center ml-auto mr-auto bg-gray-700">
+    <input name="enter value" type="text" placeholder="Enter your text" className="outline-1 px-4 py-2"></input>
+    <div className="mt-2">
+    <p>MIRRORED TEXT.</p>
+    </div>
+
+  </div>;
+};
+
+export default Mirror_text;
