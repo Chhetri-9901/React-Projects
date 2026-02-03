2b54b6c30488747ad12964e75b693f8d8fc89430
Chhetri-9901 <vivekchhetri9901@gmail.com>
2026-02-03 22:15:18 +0530

    mirror_text completed

--- react-mastersXYZ/src/components/Mirror_text.tsx
+++ react-mastersXYZ/src/components/Mirror_text.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Mirror_text = () => {
+  const [value, setValue] = useState("Waiting for input...")
+  const [valueCount, setValueCount] = useState(0)
+  function typing(e) {
+    setValue(e.target.value);
+    setValueCount((e.target.value).length); 
+  }
   return <div className="max-w-xl w-100% mt-4 border-white p-4 flex flex-col justify-center ml-auto mr-auto bg-gray-700">
-    <input name="enter value" type="text" placeholder="Enter your text" className="outline-1 px-4 py-2"></input>
-    <div className="mt-2">
-    <p>MIRRORED TEXT.</p>
+    <input 
+      name="enter value" 
+      type="text" 
+      placeholder="Enter your text" 
+      className="outline-1 px-4 py-2"
+      onChange={(e)=>{typing(e)}}
+    >
+
+    </input>
+    <div className="mt-2 bg-gray-300 p-3 rounded-2xl">
+    <p className="text-black font-bold">MIRRORED TEXT.</p>
+    <p className="text-gray-800 font-bold">{valueCount} characters</p>
+    <p className="text-gray-700 italic wrap-break-word">{value}</p>
     </div>
 
   </div>;
